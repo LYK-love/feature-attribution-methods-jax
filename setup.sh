3996202167d295aa5002c1682b3d97aa5aa14313
LYK-love <lykuni@ucdavis.edu>
2024-07-24 23:41:09 +0000

    fix

--- setup.sh
+++ setup.sh
@@ -1,10 +1,12 @@
 pip install -U "jax[cuda12]"
 pip install -q clu==0.0.12 ml-collections==0.1.1 keras==3.4.1 flax==0.8.5
 pip install jupyter==1.0.0
+pip install ipykernel==6.29.5
 pip install pandas==2.2.2
 pip install matplotlib==3.9.1
 
 # Install my customized flaxmodels
+git submodule update --init --recursive
 cd ./flaxmodels
 pip install -e .
 cd ..
